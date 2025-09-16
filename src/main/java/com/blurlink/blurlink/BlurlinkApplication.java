1b081b8244061773609f700382e147b0a558788f
basiliok <paledot01@gmail.com>
2025-09-15 19:37:47 -0500

    initial commit

---
+++ src/main/java/com/blurlink/blurlink/BlurlinkApplication.java
@@ -0,0 +1,13 @@
+package com.blurlink.blurlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlurlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlurlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blurlink/blurlink/BlurlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blurlink.blurlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlurlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
